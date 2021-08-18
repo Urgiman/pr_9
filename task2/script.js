53a911acab4aed456ee40dc21edaa403d9cd3ec3
Urgiman <urgiman@ya.ru>
2021-08-18 22:20:46 +0300

    first commit

---
+++ task2/script.js
@@ -0,0 +1,6 @@
+const  consoleLogAlert = document.querySelector('#consolLog');
+consoleLogAlert.addEventListener('click', 
+  (event) => {
+    alert('Используется для вывода информации в консоль');
+    
+})
